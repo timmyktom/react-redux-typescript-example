8fbc62e0492ff29139469b031cd87290aaa1d507
tomti <tomti@combiz.com.au>
2017-11-15 12:56:10 +1100

    Updated for Link

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 
 import './HomePage.css';
 class HomePage extends React.Component {
@@ -10,10 +11,14 @@ class HomePage extends React.Component {
           </div>
           <div className="row modules">
             <div className="col-xs-12 col-sm-6 col-md-6" >
-                <img src={process.env.PUBLIC_URL + '/assets/icon-books.jpg'}/>
+              <Link to='/books'>
+                <img src={process.env.PUBLIC_URL + '/assets/icon-books.jpg'} />
+              </Link>
             </div>
             <div className="col-xs-12 col-sm-6 col-md-6">
+            <Link to='/cars'>
                 <img src={process.env.PUBLIC_URL + '/assets/icon-cars.jpg'}/>
+            </Link>
             </div>
         </div>
       </div>
